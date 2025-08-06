5fa0778ec97b0bbbd830abffd8d39966de69013d
shabu3k <40868885+shabu3k@users.noreply.github.com>
2025-08-06 14:47:03 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull lotus201317/simple-python-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 lotus201317/simple-python-app:latest
