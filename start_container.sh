aef6e5d3107339d79210807786d1271c933a59bf
shabu3k <40868885+shabu3k@users.noreply.github.com>
2025-08-09 00:34:19 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull lotus201317/sample-python-flask-service
 # Run the Docker image as a container
-docker run -d -p 5000:5000 lotus201317/simple-python-app
+docker run -d -p 5000:5000 lotus201317/sample-python-flask-service
