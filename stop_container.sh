14d331776c8e80e14adbed67176ec66459534dd7
shabu3k <40868885+shabu3k@users.noreply.github.com>
2025-08-11 19:34:21 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -3,9 +3,5 @@ set -e
 # Get the container ID(s) of running containers
 containerID=$(docker ps -q)
 # Stop the running container (if any)
-if [ -n "$containerID" ]; then
-    echo "Stopping and removing container(s): $containerID"
-    docker rm -f $containerID
-else
-    echo "No running containers found."
-fi
+containerID=$(docker ps | awk 'NR>1 {print $1}')
+docker rm -f $containerID
